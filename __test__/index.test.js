35bd86337105fbdc1970837695bb41bc650fb15a
root <root@DESKTOP-2SDDG69.localdomain>
2022-08-16 11:30:26 +0300

    test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,7 +2,7 @@ import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import * as fs from 'fs';
-import gendiff from '../src/gendiff.js';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
